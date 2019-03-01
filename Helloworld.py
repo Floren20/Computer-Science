669a317f62dfcb7e798d3bc7c456fb615f7ced7b
Floren20 <48135933+Floren20@users.noreply.github.com>
2019-03-01 14:02:40 -0500

    Add files via upload

---
+++ Helloworld.py
@@ -0,0 +1,31 @@
+# Created by: Flore
+# Created on: 07-Feb-2019
+# Created for: ICS3U
+# Daily Assignment  Unit 0-01
+# This program displays Hello World but with an menu to exit
+from tkinter import *
+
+class Application(Frame):
+    def createWidgets(self):
+        self.QUIT = Button(self)
+        self.QUIT["text"] = "QUIT"
+        self.QUIT["fg"]   = "red"
+        self.QUIT["command"] =  self.quit
+
+        self.QUIT.pack({"side": "left"})
+
+        self.hi_there = Button(self)
+        self.hi_there["text"] = "Hello",
+        self.hi_there["command"] = self.say_hi
+
+        self.hi_there.pack({"side": "left"})
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
+
+root = Tk()
+app = Application(master=root)
+app.mainloop()
+root.destroy()
